f0a8a6c2c012dd1b47d39a0465305d3bae044c25
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-26 13:36:36 -0600

    string practice & concatenation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String myString = "This is a string! ";
+        System.out.println("My first string is: " + myString);
+        myString = myString + "Now I have 2 sentences!!";
+        System.out.println("My longer string is: " + myString);
     }
 }
